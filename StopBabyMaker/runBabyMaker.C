6bba998aaf4e2917acc7fabb7c57e4ddced3fdd4
Mia Liu <miaoyuan.liu0@gmail.com>
2015-06-25 01:45:50 -0700

    update skim

--- StopBabyMaker/runBabyMaker.C
+++ StopBabyMaker/runBabyMaker.C
@@ -49,7 +49,7 @@ int main(int argc, char **argv){
   babyMaker *mylooper = new babyMaker();
 
   //void setSkimVariables(int nvtx, float met, int nlep, float el_pt, float el_eta, float mu_pt, float mu_eta, int njets, float jetpt, float jeteta);
-  mylooper->setSkimVariables(1, 30., 1, 40., 2.1, 30., 2.1, 2, 30., 2.4);
+  mylooper->setSkimVariables(1, 30., 1, 15., 2.1, 15., 2.1, 2, 30., 2.4);
 
   int nevents = -1;
   if(argc>2) nevents = atoi(argv[2]);  
