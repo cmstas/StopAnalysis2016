360c6c90608c5b1f98cf6e25d2db0cc4082662b1
haweber <hannsjorg.artur.weber@cern.ch>
2016-10-28 09:55:58 -0700

    commit CombineAnalysis

---
+++ CombineAnalysis/README.md
@@ -0,0 +1,46 @@
+CombineAnalysis for stop1l
+
+* makeDataCards.C: make single-bin datacards: need input files from all backgrounds, data and signal to work
+- the functions creating datacards (either a single one- or a group up to all scans for all mass points) are the functions called makeDataCards*.
+All other functions are helper functions
+- Usually I use makeDataCardsOneScan() that makes all datacards for one scan (e.g. T2tt) for one analysis (e.g. general or compressed): The analysisID has however to be defined. Currently only the general/standard analysis is defined.
+- All default settings for flags is what we also use as default.
+- functions called analyis* are specific to each analysis (e.g. general) and need to be defined beforehand.
+- For filenames/histogram names, you need to go through the load* functions, the main makeDataCardsOneBinBasis() function, as well as the UpdatePlottingHisto() - that last one makes histograms for plotting/table making.
+
+--> Currently, if one histogram name is spelled wrongly, the code just crashes.
+Also if the file/histogram defined in UpdatePlottingHisto() does not exist, the code crashes.
+
+
+* CombineCards.sh: combine all SR of single-bin datacards into a combined datacard.
+- Please note, that the combinedcards/ need to exists, else the program will literally do nothing
+- You need to define signal such as "std_T2tt_850_150" as argument
+- the directory where the single-bin datacards are stored is hardcoded "thedir"
+- the maximal number of bins is hardcoded - in principle one could set this very high - program will just run slower. Should make this an argument
+
+* CombineCardsScan.sh: helper function to run on a complete scan. There is a bunch of hardcoded stuff, but you can overwrite those with arguments.
+- some variables don't do anything, as the script uses those defined in CombineCards.sh
+
+*CombineCardsAllScans.sh: obvious
+
+* Make2DSignifHistos.C: out of the Combine output make 2D histograms with p-value and significances (sigma). Currently written that you need to run expected priori/posteriori and observed first.
+- in here, the signaltype string also contains analysisID, e.g. "std_T2tt"
+- main function is Make2DSignifHistos() with default flags being correct.
+- expect as input the combine output tars as done by the batch submission scripts in the subdirectories.
+
+* Make2DLimitHistos.C: as Make2DSignifHistos.C, but for limits
+- in here, the signaltype string also contains analysisID, e.g. "std_T2tt"
+- main function is Make2DLimitHistos () with default flags being correct.
+- expect as input the combine output tars as done by the batch submission scripts in the subdirectories.
+
+* CovarianceAndCorrelation: Out of a set of one big datacard containing all SR bins (signal is not important here - only background), create combine output that allows to make a covariance matrix histogram, that then can be transformed into a correlation matix histogram.
+- This one does not run on a tagged version of combine - need untagged version...
+
+* batchlimit: batch submission for running combine on condor using datacards:
+- setup.sh: Define all the ALLCAPS variables on top
+- batch.sh: This is what you need to run, but define before "NAME" (analyisID_ScanName) and "postfit" (true used for final results, for studies use false)
+- cleanup.sh: each time you run batch.sh - this will create tar files from the datacarddir - WAIT until all your jobs are running, then you can cleanup.
+ALSO WAIT before rerunning batch.sh until all jobs are running.
+
+* batchsignificance: batch submission for running combine on condor using datacards
+- similar to batchlimit - in batch.sh instead of "postfit" you need to define "observed" and "posteriori"
---
+++ CombineAnalysis/batchsignificance/condorExecutable.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+
+# Track time job begins
+RIGHT_NOW=`date +"%Y%m%d %H%M%S"`
+echo ""
+echo "*************************************"
+echo "  Beginning Condor Job "
+echo "    TIME = $RIGHT_NOW "
+echo "*************************************"
+echo ""
+
+# This stuff to drive the the limit macro
+#only input I need to define is
+posteriori=$1
+observed=$2
+
+#This stuff to get output back
+export COPYDIR=$3
+export CONDOR_DIR_NAME=$4
+export DATACARDS_TAR=$5
+export MAKER_NAME=$6
+
+
+# Setup Environment
+RIGHT_NOW=`date +"%Y%m%d %H%M%S"`
+echo ""
+echo "*************************************"
+echo "  Setting up environment... "
+echo "    TIME = $RIGHT_NOW "
+echo "*************************************"
+echo ""
+
+# Show where you are
+hostname
+
+# Show g++ version
+echo " " 
+echo "G++ INFORMATION" 
+g++ --version
+which g++
+
+CMSSW_VERSION=CMSSW_7_1_5
+
+
+
+# Untar the zip dir
+RIGHT_NOW=`date +"%Y%m%d %H%M%S"`
+echo ""
+echo "*************************************"
+echo "  Unpack the tarball... "
+echo "    TIME = $RIGHT_NOW "
+echo "*************************************"
+echo ""
+
+echo "This this the current dir: "
+THISDIR=`pwd`
+echo $THISDIR
+#echo "LS to check that all files were transferred: "
+#ls
+tar xzf $CONDOR_DIR_NAME.tar.gz
+#echo "LS to check that tarball was unpacked: "
+#ls
+#cd $CONDOR_DIR_NAME
+#echo "LS to check contents of unpacked stopbabymaker: "
+#ls
+
+OLDDIR=`pwd`
+#This should be CMSSW_7_1_5/src/HiggsAnalysis/CombinedLimit/
+cd $MAKER_NAME/
+
+export CMS_PATH=/cvmfs/cms.cern.ch
+echo "CMSENV"
+export SCRAM_ARCH=slc6_amd64_gcc481
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+#cd 
+eval `scramv1 runtime -sh`
+cd $OLDDIR
+echo "copy datacards into HiggsCombine directory"
+cp $DATACARDS_TAR $MAKER_NAME/.
+cd $MAKER_NAME/
+echo "rescram to update links"
+scramv1 b ProjectRename
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scramv1 runtime -sh`
+echo "untar datacards"
+tar xzfv $DATACARDS_TAR
+#echo "LS to check contents of unpacked combine: "
+#ls
+echo "remove the tar files "
+rm $CONDOR_DIR_NAME.tar.gz
+rm $DATACARDS_TAR
+
+
+# Run babyMaker
+RIGHT_NOW=`date +"%Y%m%d %H%M%S"`
+echo ""
+echo "*************************************"
+echo "  Running combine limits... "
+echo "    TIME = $RIGHT_NOW "
+echo "*************************************"
+echo ""
+
+methodcmd=""
+methodname="Priori"
+if [ "${posteriori}" = true ]
+then
+    methodcmd="--toysFreq"
+    methodname="Posteriori"
+fi
+
+#get all datacards
+NameArray=( )
+for f in *; do
+    if [ ${f: -7} == ".tar.gz" ]
+    then
+	continue
+    fi
+    #echo $f
+    mytest=`echo $f | grep -q datacard ; echo $?`
+    #echo "mytest $mytest"
+    if [ $mytest == "0" ]
+    then
+        #echo "passed $f"
+        NameArray=("${NameArray[@]}" "${f}")
+    fi
+done
+echo "${NameArray[@]}"
+
+#run over all datacards
+for Name in "${NameArray[@]}"
+do
+    filenamenoext="${Name%.*}"
+    stripone=`echo "datacard_"`
+    #echo $stripone
+    Signal=`echo ${filenamenoext#${stripone}}`
+    #echo "get limit for $Signal"
+    #computes automatically observed and expected limit together
+    if [ "${observed}" = true ]
+    then
+	echo "runing combine  -M ProfileLikelihood --significance ${Name} -n Obs${Signal} > /dev/null 2>&1"
+	combine  -M ProfileLikelihood --uncapped 1 --rMin -50 --rMax 50 --significance ${Name} -n Obs${Signal} #> /dev/null 2>&1
+	#ls
+	mv higgsCombineObs${Signal}.ProfileLikelihood.mH120.root Signif_ProfileLikelihood_Obs_${Signal}.root
+	#ls
+    else
+	echo "runing  combine  -M ProfileLikelihood --significance ${Name} -n Exp${methodname}${Signal} -t -1 --expectSignal=1 ${methodcmd} > /dev/null 2>&1"
+	combine  -M ProfileLikelihood --uncapped 1 --rMin -50 --rMax 50 --significance ${Name} -n Exp${methodname}${Signal} -t -1 --expectSignal=1 ${methodcmd} #> /dev/null 2>&1
+	#ls
+	mv higgsCombineExp${methodname}${Signal}.ProfileLikelihood.mH120.root Signif_ProfileLikelihood_Exp${methodname}_${Signal}.root
+	#ls
+    fi
+    #> /dev/null 2>&1
+done
+
+
+# Copy the output to the output directory via lcg-cp
+RIGHT_NOW=`date +"%Y%m%d %H%M%S"`
+echo ""
+echo "*************************************"
+echo "  Copy output file with lcg-cp... "
+echo "    TIME = $RIGHT_NOW "
+echo "*************************************"
+echo ""
+
+#  This preserves grid functionality
+#echo "copying.  LS is: "
+#ls -l *.root
+#ls
+rootfilearray=( )
+for f in *; do
+    #echo $f
+    mytest=`echo $f | grep -q root ; echo $?`
+    #echo "mytest $mytest"
+    if [ $mytest == "0" ]
+    then
+        #echo "passed $f"
+        #rootfilearray=("${rootfilearray[@]}" "${f}")
+	rootfilearray=`echo ${f} ${rootfilearray}`
+    fi
+done
+#echo "${rootfilearray[@]}"
+
+outname=`echo tar_Signif_ProfileLikelihood_Exp${methodname}$7.tar.gz`
+if [ "${observed}" = true ]
+then
+    outname=`echo tar_Signif_ProfileLikelihood_Obs$7.tar.gz`
+fi
+echo "tar -czvf ${outname} ${rootfilearray}"
+tar -czvf ${outname} ${rootfilearray}
+#echo "${rootfilearray[@]}"
+
+#for Name in "${rootfilearray[@]}"
+#do
+#    #echo "Transfer ${Name}"
+#    lcg-cp -b -D srmv2 --vo cms --connect-timeout 2400 --verbose file://`pwd`/${Name} srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${COPYDIR}/${Name}
+#done
+echo "Tranfer ${COPYDIR}/${outname}"
+#lcg-cp -b -D srmv2 --vo cms --connect-timeout 2400 --verbose file://`pwd`/${outname} srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${COPYDIR}/${outname}
+gfal-copy -f file://`pwd`/${outname} srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=${COPYDIR}/${outname}
+
+# Job Complete
+RIGHT_NOW=`date +"%Y%m%d %H%M%S"`
+echo ""
+echo "*************************************"
+echo "  JOB COMPLETE! "
+echo "    TIME = $RIGHT_NOW "
+echo "*************************************"
+echo ""
