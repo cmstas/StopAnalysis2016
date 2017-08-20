f266c95bf75bb1995edd830c23c325b0dd3bb48b
wsicheng <sicheng@physics.ucsb.edu>
2017-08-20 12:15:56 -0700

    Fix seg-fault at end by unlinking jetcorr.o

--- StopLooper/runStopLooper.cc
+++ StopLooper/runStopLooper.cc
@@ -31,8 +31,5 @@ int main(int argc, char** argv)
   StopLooper stop;
   stop.looper(ch, sample, output_dir); 
 
-  cout << __FILE__ << ':' << __LINE__ << ": Fine until here! " << endl;
-  throw std::range_error("Why there's an annoying segmentation fault here!!!");
-  
   return 0;
 }
